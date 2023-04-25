d0215c8abd6d5db9ccccab8aaefd30f377b65014
kritebh <kritebhlaganbibhakar@gmail.com>
2023-04-25 10:17:03 +0530

    tsc readme.md

---
+++ README.md
@@ -0,0 +1,38 @@
+- Browser doesn't understand typescript
+- There is a transpiler which convert typescript to javascript
+- Javascript is dynamic language which is good and bad at the same time
+- Typescript is same as javascript but with "data type" and extra features similar to C++
+
+
+## Data Type in Typescript
+  Number
+  String
+  Boolean
+  NULL
+  undefined
+  symbol
+  Object
+
+## Object
+
+```typescript
+let user:{
+    name:string;
+    email:string;
+} = {
+    name:'klb',
+    email:'email@mail.com'
+}
+```
+## Array
+
+```typescript
+let fruits:string[];
+```
+## Boolean
+
+```typescript
+let isReload :boolean = true
+```
+
+
---
+++ ep01/js/main.js
@@ -0,0 +1,23 @@
+// let first = document.querySelector("#first") as HTMLInputElement
+// let second = document.querySelector("#second") as HTMLInputElement
+// let addButton = document.querySelector("#add") 
+// let ans = document.querySelector("#ans") 
+// function add(num1:number,num2:number) {
+//     let sol = num1+ num2
+//     console.log(sol)
+//     ans.innerHTML = sol
+// }
+// addButton.addEventListener("click", ()=>{add(+first.value,+second.value)});
+// let user:{
+//     name:string;
+//     email:string;
+// } = {
+//     name:'klb',
+//     email:'email@mail.com'
+// }
+// console.log(user.name)
+// let fruits:string[];
+// fruits = ['mango','apple']
+// console.log(fruits)
+var isReload = true;
+console.log(isReload);
